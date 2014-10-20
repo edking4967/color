c8b62fcb0f0b9137687c822a5aa8847240a6f38e
Edward King <ed.king4967@gmail.com>
2014-10-19 20:22:31 -0600

    Phase 1 complete

--- color.py
+++ color.py
@@ -14,8 +14,16 @@ extn = '.colors'  # ".color" files define all the colors in a swatch
 
 #An example
 fall = open( 'fall' + extn , 'r')
-print( fall.read() ) 
+
+#TODO: parse strings / split strings
+
+lines = fall.read()
 
 
+lines_data = filter(None, lines.split('\n'))
 
-
+for line in lines_data:
+  words = line.split(' ')
+  rgb = words[0]
+  name = words[1]
+  print 'rgb= ' + rgb + ' ' + 'name= ' + name 
