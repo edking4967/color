971923ecd009f5a33d67fc33f1c450e3d8c8d641
Your Name <you@example.com>
2014-10-21 14:55:02 -0500

    Added prototype for a terminal display output

--- color.py
+++ color.py
@@ -3,19 +3,21 @@ from os import path
 
 class Color:
   name = "white"
-  rgb= 0xffffff 
+  rgb= 0xffffff
 
 class Swatch:
   color = Color
 
-
+# Display output modules:
+def displayInTerminal():
+  return;
+# Enough display output modules
 
 extn = '.colors'  # ".color" files define all the colors in a swatch
 
 #An example
 fall = open( 'fall' + extn , 'r')
 
-#TODO: parse strings / split strings
 
 lines = fall.read()
 
@@ -26,4 +28,4 @@ for line in lines_data:
   words = line.split(' ')
   rgb = words[0]
   name = words[1]
-  print 'rgb= ' + rgb + ' ' + 'name= ' + name 
+  print 'rgb= ' + rgb + ' ' + 'name= ' + name
