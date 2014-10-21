9ae844d7ad21fc025b3b67feb0b0709190a011f1
Your Name <you@example.com>
2014-10-21 17:27:25 -0500

    started to generalize with functions

--- color.py
+++ color.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python
 from os import path
 
+# Strings and shortcuts
+extn = '.colors'  # ".color" files define all the colors in a swatch
+
+# A color.
 class Color:
   name = "white"
   rgb= 0xffffff
-
+# A swatch contains multiple colors.
 class Swatch:
   color = Color
 
 # Display output modules:
 def displayInTerminal():
+  return;  # Mind seems stuck in java
+def outputInCSS():
   return;
 # Enough display output modules
 
-extn = '.colors'  # ".color" files define all the colors in a swatch
+# The main:
+def processColors( color ): # Now we are in C it seems
 
-#An example
-fall = open( 'fall' + extn , 'r')
+  fall = open( 'fall' + extn , 'r')
+  lines = fall.read()
+  lines_data = filter(None, lines.split('\n'))
 
+  for line in lines_data:
+    words = line.split(' ')
+    rgb = words[0]
+    name = words[1]
+    print 'rgb= ' + rgb + ' ' + 'name= ' + name
+    return;
 
-lines = fall.read()
+# An example
+color = open( 'fall' + extn , 'r')
 
+processColors( 'fall' )
 
-lines_data = filter(None, lines.split('\n'))
-
-for line in lines_data:
-  words = line.split(' ')
-  rgb = words[0]
-  name = words[1]
-  print 'rgb= ' + rgb + ' ' + 'name= ' + name
